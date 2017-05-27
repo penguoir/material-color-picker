fd359b8276ede7eef088edd2d428e2747e518b35
penguoir <games@marash.net>
2017-05-27 23:23:03 +0100

    Main App

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./scss/main.scss")
+        .pipe(sass())
+        .pipe(prefix())
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
